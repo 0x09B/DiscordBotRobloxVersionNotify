16006946c6ad75a1789f3bf47d9cf31c42a8eb5c
GameOverAgain <nusret.balacayev@gmail.com>
2020-11-26 01:30:17 +0400

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ import json
 from datetime import date
 from discord.utils import get
 from discord.ext.commands import check, MissingRole, CommandError
+intents = discord.Intents.all()
 bot_config = json.loads(open("config.json", "r").read())
-client = commands.Bot(command_prefix=bot_config["bot_prefix"])
-client.remove_command("help")
+client = commands.Bot(command_prefix=bot_config["bot_prefix"],intents=intents)
 channeltopost = bot_config["channel_id_to_send_boblox_updates"]
 
 @client.event
@@ -26,7 +26,7 @@ async def ping(ctx):
 async def cum(ctx):
 	await ctx.send("Onii-Chan Don't Cum Inside Kyaa~😊")
 
-@tasks.loop(seconds=1.0)
+@tasks.loop(seconds=60)
 async def my_loop():
 	a = requests.get('http://setup.roblox.com/version')
 	time.sleep(10)
